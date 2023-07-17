8fb362e2a67535bb3954e43755db18feb26b6405
Mücahit <76880536+mucahitdev@users.noreply.github.com>
2023-07-18 00:10:03 +0300

    navigation settings

--- App.tsx
+++ App.tsx
@@ -1,24 +1,31 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 
 import { NativeBaseProvider } from "native-base";
 import { Provider } from 'react-redux';
 import { store } from 'store/store';
 
 import AppNavigations from 'navigations/AppNavigations';
+import Color from 'common/color';
 
 
 export default function App() {
 
   return (
-    <SafeAreaProvider>
-      <NativeBaseProvider>
-        <Provider store={store}>
-          <NavigationContainer>
-            <AppNavigations />
-          </NavigationContainer>
-        </Provider>
-      </NativeBaseProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }} >
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: Color.secondary }}>
+          <NativeBaseProvider>
+            <Provider store={store}>
+              <NavigationContainer>
+                <AppNavigations />
+              </NavigationContainer>
+            </Provider>
+          </NativeBaseProvider>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
--- src/navigations/AppNavigations/index.tsx
+++ src/navigations/AppNavigations/index.tsx
@@ -3,18 +3,13 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useSelector } from 'react-redux';
 import Color from 'common/color';
 
+// Screens
+import { ScanScreen } from 'screens/index';
+
 // Icons
 import { FontAwesome } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
-
-function HomeScreen() {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  );
-}
 
 function SettingsScreen() {
   return (
@@ -32,9 +27,21 @@ export default function AppNavigations() {
         headerShown: false,
         tabBarActiveTintColor: Color.primary,
         tabBarInactiveTintColor: Color.gray,
-      }
-    }>
-      <Tab.Screen name="Tara" component={HomeScreen}
+        tabBarStyle: {
+          backgroundColor: Color.secondary,
+          height: 50
+        },
+        tabBarLabelStyle: {
+          paddingBottom: 5
+        }
+
+      }}
+      sceneContainerStyle={{
+        backgroundColor: Color.secondary,
+      }}
+
+    >
+      <Tab.Screen name="Scan" component={ScanScreen}
         options={{
           tabBarIcon: ({ color, size }) => (
             <FontAwesome name="qrcode" size={size} color={color} />
@@ -42,7 +49,7 @@ export default function AppNavigations() {
         }}
       />
       {saveHistory &&
-        <Tab.Screen name="Geçmiş" component={SettingsScreen}
+        <Tab.Screen name="History" component={SettingsScreen}
           options={{
             tabBarIcon: ({ color, size }) => (
               <FontAwesome name="history" size={size} color={color} />
@@ -51,7 +58,7 @@ export default function AppNavigations() {
         />
       }
       {saveLocalStorage &&
-        <Tab.Screen name="Kayıtlar" component={SettingsScreen}
+        <Tab.Screen name="Records" component={SettingsScreen}
           options={{
             tabBarIcon: ({ color, size }) => (
               <FontAwesome name="save" size={size} color={color} />
@@ -59,7 +66,7 @@ export default function AppNavigations() {
           }}
         />
       }
-      <Tab.Screen name="Ayarlar" component={SettingsScreen}
+      <Tab.Screen name="Settings" component={SettingsScreen}
         options={{
           tabBarIcon: ({ color, size }) => (
             <FontAwesome name="cog" size={size} color={color} />
